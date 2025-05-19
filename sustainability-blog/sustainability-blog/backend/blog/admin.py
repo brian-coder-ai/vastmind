205da96870d878fa45afee2bd423a2341cf9d0c9
brian-coder-ai <korirb289@gmail.com>
2025-05-19 13:46:14 +0300

    Update admin.py

--- sustainability-blog/sustainability-blog/backend/blog/admin.py
+++ sustainability-blog/sustainability-blog/backend/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Tag, Post
+from .models import Category, Tag, Post,ArticleImage
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
